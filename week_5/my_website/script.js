f70d68db26746f081ed3f07d5e3d38083afd5e8b
julianagreen <juliana@stickpony.net>
2016-07-27 16:56:44 -0700

    website day 3

--- week_5/my_website/script.js
+++ week_5/my_website/script.js
@@ -1,25 +1,54 @@
-window.onload= function() {
-    $('.drop-item').on('click', resetDisplay)
-    // $('.drop-item').each(clickListener);
-}
+$(document).ready(function(){
+    $('.drop-item').on('click', resetDisplay);
+    $(".nav_item").on("click", nav_to_page);
+
+    fade_now();
+});
 
 function resetDisplay(event){
     $(".project-section").hide();
-    var dropItem = event.target.innerHTML;
+    var dropItem = $(this).html();
+    // var dropItem = event.target.innerHTML;
     if (dropItem == "Art Books"){
         $("#books").show();
     }
-    if (dropItem == "letterpress"){
+    if (dropItem == "Letterpress"){
         $("#letterpress").show();
     }
     if (dropItem == "Wood Projects"){
         $("#wood").show();
     }
-    // if (dropItem == "Architecture"){
-    //     $("#architecture").show();
-    // }
+    if (dropItem == "Architecture"){
+        $("#architecture").show();
+    }
+    if (dropItem == "Bracelets"){
+        $("#bracelets").show();
+    }
+    if (dropItem == "Other"){
+        $("#other").show();
+    }
 }
 
 function myFunction(){
     document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+function fade_now(){
+    var hover_target = $(".project").children("img");
+    $(hover_target).mouseenter(function(){
+        $(this).fadeTo("slow", .3);
+        // $("#date").show();
+    });
+
+    $(hover_target).mouseleave(function(){
+        $(this).fadeTo("slow", 1);
+        // $("#date").hide();
+    });
+}
+
+function nav_to_page(){
+    $(".page").hide();
+    var which_page = "#" + $(this).html();
+    $(which_page).show();
 }
